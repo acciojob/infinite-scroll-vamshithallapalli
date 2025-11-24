5728c972d3f96fb84a3d280759718910d5f19696
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-11-24 07:39:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Select the list element
-const list = document.getElementById("infilist");
+const list = document.getElementById("infi-list");
 
 // Keep track of how many items are currently in the list
 let itemCount = 0;
